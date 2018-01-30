79c8e632aaa9eb0147b1073eadc45153e9e0e8b4
Hung1610 <phuochung008@gmail.com>
2018-01-30 15:50:53 +0700

    Gay

---
+++ src/function.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class function {
+	public void GiuXe() {
+		try {
+			FileReader f1= new FileReader("input1.txt");
+			BufferedReader r1= new BufferedReader(f1);
+			FileReader f2= new FileReader("input2.txt");
+			BufferedReader r2= new BufferedReader(f2);
+			FileWriter f3= new FileWriter("output.txt");
+			PrintWriter o= new PrintWriter(f3);
+			SimpleDateFormat f= new SimpleDateFormat("HH:mm dd/MM/yyyy");
+			
+			while(true) {
+				String st1=r1.readLine();
+				if(st1==""||st1==null) break;
+				String[] ds1=st1.split("[;]");
+				String st2=r2.readLine();
+				if(st2==""||st2==null) break;
+				String[] ds2=st2.split("[;]");
+				Date d1=f.parse(ds1[3]);
+				Date d2=f.parse(ds2[3]);
+				long dif= d2.getTime()-d1.getTime();
+				long bike = dif/86400000;
+				if (ds1[0].equals("4")) {
+					long car = (dif/1800000)*5000;
+					String tien= Long.toString(car);
+					if(!ds2[4].equalsIgnoreCase("binh thuong")||!ds2[4].equalsIgnoreCase("not available"))
+						o.println(ds1[0]+";"+ds1[1]+";"+ds1[2]+";"+ds1[3]+";"+ds2[3]+";"+"Chua xac dinh - Dang xu ly boi thuong");
+					else o.println(ds1[0]+";"+ds1[1]+";"+ds1[2]+";"+ds1[3]+";"+ds2[3]+";"+tien);
+				}
+				else if (ds1[0].equals("2")) {
+					long motor = bike*3000;
+					String tien = Long.toString(motor);
+					o.println(ds1[0]+";"+ds1[1]+";"+ds1[2]+";"+ds1[3]+";"+ds2[3]+";"+tien);
+				}
+				else {
+					long dap = bike*1000;
+					String tien = Long.toString(dap);
+					o.println(ds1[0]+";"+ds1[1]+";"+ds1[2]+";"+ds1[3]+";"+ds2[3]+";"+tien);
+				}
+			}
+			r1.close();
+			r2.close();
+			o.close();
+		}catch(Exception tt) {
+			System.out.println("Loi:"+ tt.getMessage());
+		}
+	}
+}
